ad67f0688878cf42e196d4c8dceb4e4c5589aff4
Artem Venger <artem.venger.professional@gmail.com>
2025-05-12 13:55:55 +0700

    Create base destroy task

---
+++ Assets/Game/Code/Task/DestroyTaskBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public abstract class DestroyTaskBase : ITask
+    {
+        private readonly CollisionRoom _collisionRoom;
+
+        protected DestroyTaskBase(CollisionRoom collisionRoom, int targetCount)
+        {
+            _collisionRoom = collisionRoom;
+            TargetCount = targetCount;
+            _collisionRoom.FigureDestroyed += FigureDestroyedEventHandler;
+        }
+
+        public float Progress { get; private set; }
+
+        public abstract string DisplayName { get; }
+
+        public event Action<float> ProgressChanged;
+
+        protected int TargetCount { get; }
+
+        protected int DestroyedFiguresCount { get; private set; }
+
+        protected abstract bool CanIncrementCounter(FigureType figure);
+
+        private void FigureDestroyedEventHandler(FigureType figure)
+        {
+            if (CanIncrementCounter(figure) == false)
+            {
+                return;
+            }
+
+            DestroyedFiguresCount++;
+            Progress = (float)DestroyedFiguresCount / TargetCount;
+            if (Mathf.Approximately(Progress, b: 1))
+            {
+                _collisionRoom.FigureDestroyed -= FigureDestroyedEventHandler;
+            }
+            ProgressChanged?.Invoke(Progress);
+        }
+    }
+}
