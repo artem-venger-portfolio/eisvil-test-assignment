a259b4f99cdd944fe2a992d34138d6437051e3af
Artem Venger <artem.venger.professional@gmail.com>
2025-05-12 13:11:45 +0700

    Create DestroyAnyTask

---
+++ Assets/Game/Code/Task/DestroyAnyTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class DestroyAnyTask : ITask
+    {
+        private readonly CollisionRoom _collisionRoom;
+        private readonly int _targetCount;
+        private int _destroyedFiguresCount;
+
+        public DestroyAnyTask(CollisionRoom collisionRoom, int targetCount)
+        {
+            _collisionRoom = collisionRoom;
+            _targetCount = targetCount;
+            _collisionRoom.FigureDestroyed += FigureDestroyedEventHandler;
+        }
+
+        public float Progress { get; private set; }
+
+        public string DisplayName => "Any object destroyed";
+
+        public event Action<float> ProgressChanged;
+
+        private void FigureDestroyedEventHandler(FigureType figure)
+        {
+            Progress = (float)_destroyedFiguresCount / _targetCount;
+            if (Mathf.Approximately(Progress, b: 1))
+            {
+                _collisionRoom.FigureDestroyed -= FigureDestroyedEventHandler;
+            }
+            ProgressChanged?.Invoke(Progress);
+        }
+    }
+}
---
+++ Assets/Game/Code/Task/ITask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Game
+{
+    public interface ITask
+    {
+        public string DisplayName { get; }
+        public float Progress { get; }
+        public event Action<float> ProgressChanged;
+    }
+}
