697f8276597188fbd5c7f17e6631289f55ddddde
Artem Venger <artem.venger.professional@gmail.com>
2025-05-12 10:19:10 +0700

    Get Rigidbody

--- Assets/Game/Code/Figure/Figure.cs
+++ Assets/Game/Code/Figure/Figure.cs
@@ -2,10 +2,18 @@
 
 namespace Game
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Figure : MonoBehaviour
     {
         [SerializeField]
         private FigureType _type;
+
+        private Rigidbody2D _rigidbody;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
 
         public FigureType Type => _type;
     }
