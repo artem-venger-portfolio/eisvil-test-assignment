d9a4f4cdd0040c75b70317380c2d026ae059f90e
Artem Venger <artem.venger.professional@gmail.com>
2025-05-12 13:31:08 +0700

    Create TaskView

---
+++ Assets/Game/Code/HUD/TaskView.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game
+{
+    public class TaskView : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform _fill;
+
+        [SerializeField]
+        private TMP_Text _nameField;
+
+        private ITask _task;
+        
+        public void Initialize(ITask task)
+        {
+            _task = task;
+            SetProgress(_task.Progress);
+            _task.ProgressChanged += SetProgress;
+            _nameField.text = _task.DisplayName;
+        }
+
+        private void SetProgress(float progress)
+        {
+            _fill.anchorMax = new Vector2(progress, 1);
+        }
+    }
+}
