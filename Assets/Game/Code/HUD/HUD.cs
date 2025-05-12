a6c71f30c2c6ea426e274d489ef723160c740a95
Artem Venger <artem.venger.professional@gmail.com>
2025-05-12 13:35:05 +0700

    Reference task view and task container

--- Assets/Game/Code/HUD/HUD.cs
+++ Assets/Game/Code/HUD/HUD.cs
@@ -5,6 +5,12 @@ namespace Game
 {
     public class HUD : MonoBehaviour
     {
+        [SerializeField]
+        private TaskView _taskTemplate;
+
+        [SerializeField]
+        private RectTransform _taskContainer;
+
         private IList<ITask> _tasks;
 
         public void Initialize(IList<ITask> tasks)
