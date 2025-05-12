4662931ccee481614bd76d0cb89f3c909fbb21eb
Artem Venger <artem.venger.professional@gmail.com>
2025-05-12 13:59:50 +0700

    Use DestroyTaskBase

--- Assets/Game/Code/Task/DestroyAnyTask.cs
+++ Assets/Game/Code/Task/DestroyAnyTask.cs
@@ -1,36 +1,16 @@
-﻿using System;
-using UnityEngine;
-
-namespace Game
+﻿namespace Game
 {
-    public class DestroyAnyTask : ITask
+    public class DestroyAnyTask : DestroyTaskBase
     {
-        private readonly CollisionRoom _collisionRoom;
-        private readonly int _targetCount;
-        private int _destroyedFiguresCount;
-
-        public DestroyAnyTask(CollisionRoom collisionRoom, int targetCount)
+        public DestroyAnyTask(CollisionRoom collisionRoom, int targetCount) : base(collisionRoom, targetCount)
         {
-            _collisionRoom = collisionRoom;
-            _targetCount = targetCount;
-            _collisionRoom.FigureDestroyed += FigureDestroyedEventHandler;
         }
 
-        public float Progress { get; private set; }
+        public override string DisplayName => $"Any object destroyed ({DestroyedFiguresCount}/{TargetCount})";
 
-        public string DisplayName => $"Any object destroyed ({_destroyedFiguresCount}/{_targetCount})";
-
-        public event Action<float> ProgressChanged;
-
-        private void FigureDestroyedEventHandler(FigureType figure)
+        protected override bool CanIncrementCounter(FigureType figure)
         {
-            _destroyedFiguresCount++;
-            Progress = (float)_destroyedFiguresCount / _targetCount;
-            if (Mathf.Approximately(Progress, b: 1))
-            {
-                _collisionRoom.FigureDestroyed -= FigureDestroyedEventHandler;
-            }
-            ProgressChanged?.Invoke(Progress);
+            return true;
         }
     }
 }
--- Assets/Game/Code/Task/DestroyTask.cs
+++ Assets/Game/Code/Task/DestroyTask.cs
@@ -1,43 +1,21 @@
-﻿using System;
-using UnityEngine;
-
-namespace Game
+﻿namespace Game
 {
-    public class DestroyTask : ITask
+    public class DestroyTask : DestroyTaskBase
     {
-        private readonly CollisionRoom _collisionRoom;
         private readonly FigureType _targetFigure;
-        private readonly int _targetCount;
-        private int _destroyedFiguresCount;
 
-        public DestroyTask(CollisionRoom collisionRoom, FigureType targetFigure, int targetCount)
+        public DestroyTask(CollisionRoom collisionRoom, FigureType targetFigure, int targetCount) :
+                base(collisionRoom, targetCount)
         {
-            _collisionRoom = collisionRoom;
             _targetFigure = targetFigure;
-            _targetCount = targetCount;
-            _collisionRoom.FigureDestroyed += FigureDestroyedEventHandler;
         }
 
-        public float Progress { get; private set; }
+        public override string DisplayName => $"{_targetFigure.ToString()} destroyed " +
+                                              $"({DestroyedFiguresCount}/{TargetCount})";
 
-        public string DisplayName => $"{_targetCount.ToString()} destroyed ({_destroyedFiguresCount}/{_targetCount})";
-
-        public event Action<float> ProgressChanged;
-
-        private void FigureDestroyedEventHandler(FigureType figure)
+        protected override bool CanIncrementCounter(FigureType figure)
         {
-            if (_targetFigure != figure)
-            {
-                return;
-            }
-
-            _destroyedFiguresCount++;
-            Progress = (float)_destroyedFiguresCount / _targetCount;
-            if (Mathf.Approximately(Progress, b: 1))
-            {
-                _collisionRoom.FigureDestroyed -= FigureDestroyedEventHandler;
-            }
-            ProgressChanged?.Invoke(Progress);
+            return _targetFigure == figure;
         }
     }
 }
